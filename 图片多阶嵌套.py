07cb682f1467ab76d42229bd8a168fc081d8fc10
Oslomayor <34310801+Oslomayor@users.noreply.github.com>
2017-12-08 23:02:08 +0800

    Python源码

---
+++ 图片多阶嵌套.py
@@ -0,0 +1,16 @@
+#图片多阶嵌套
+from PIL import Image
+#图片路径
+content = 'D:\Python\Python36\pictures'
+img = Image.open(content+'\boy.jpg')
+img.show()
+imgg = img
+box = (0,0,640,640)
+region = imgg.crop(box)
+for i in range(0,3):
+    region = region.resize((640-i*200,640-i*200))
+    box = (i*100,i*100)
+    imgg.paste(region,box)
+imgg.show()
+
+#imgg.save(content+'\wetchat3.jpg')
